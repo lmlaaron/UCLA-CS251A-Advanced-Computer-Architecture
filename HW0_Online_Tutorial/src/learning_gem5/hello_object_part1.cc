e86be58aead7a4ad1b5628f11e86ce2b31f53927
JonnyKong <jonnykong1996@gmail.com>
2018-10-01 11:21:13 -0700

    add Hello debug flag

--- HW0_Online_Tutorial/src/learning_gem5/hello_object_part1.cc
+++ HW0_Online_Tutorial/src/learning_gem5/hello_object_part1.cc
@@ -1,11 +1,13 @@
 #include "learning_gem5/hello_object_part1.hh"
+#include "debug/Hello.hh"
 
 #include <iostream>
 
 HelloObjectPart1::HelloObjectPart1(HelloObjectPart1Params *params) :
     SimObject(params)
 {
-    std::cout << "Hello World! From a SimObject!" << std::endl;
+    /* DPRINTF is a macro. Print to Hello debug flag */
+    DPRINTF(Hello, "Created the hello object\n");
 }
 
 HelloObjectPart1*
