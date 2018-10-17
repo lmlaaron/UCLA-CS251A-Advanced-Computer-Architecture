2796ce7531f248a13f7fd8ce6a9e35a7b72b1f8c
JonnyKong <jonnykong1996@gmail.com>
2018-10-17 11:06:46 -0700

    update result

--- HW1_Gem5_Intro/run.sh
+++ HW1_Gem5_Intro/run.sh
@@ -3,7 +3,8 @@
 GEM5_BASE=~/gem5
 SRC_DIR=~/UCLA-CS251-Advanced-Computer-Architecture/HW1_Gem5_Intro/src
 RESULT_DIR=~/UCLA-CS251-Advanced-Computer-Architecture/HW1_Gem5_Intro/result
-TARGET=~/UCLA-CS251-Advanced-Computer-Architecture/HW1_Gem5_Intro/two_level.py
+# TARGET=~/UCLA-CS251-Advanced-Computer-Architecture/HW1_Gem5_Intro/two_level.py
+TARGET=/root/gem5/configs/example/se.py
 
 NUM=1
 
@@ -20,45 +21,66 @@ for FILE in mm lfsr merge sieve; do
 
     # Default
     echo [${NUM}/28]: Task 1 [default] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=DerivO3CPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=DDR3_1600_8x8
     cp -r m5out/ ${RESULT_DIR}/${FILE}/1_default/
     ((NUM++))
 
     # MinorCPU
     echo [${NUM}/28]: Task 2 [MinorCPU] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --cpu_type='MinorCPU' --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET} --cpu_type='MinorCPU' --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=MinorCPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=DDR3_1600_8x8
     cp -r m5out/ ${RESULT_DIR}/${FILE}/2_MinorCPU/
     ((NUM++))
 
     # -O1
     cd $SRC_DIR && make clean --silent && make OPT=-O1 --silent && cd $GEM5_BASE
     echo [${NUM}/28]: Task 3 [-O1] -\> $FILE
-    build/X86/gem5.opt ${TARGET}  --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET}  --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=DerivO3CPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=DDR3_1600_8x8
     cp -r m5out/ ${RESULT_DIR}/${FILE}/3_O1/
     ((NUM++))
 
     # 4GHz
     cd $SRC_DIR && make clean --silent && make OPT=-O3 --silent && cd $GEM5_BASE
     echo [${NUM}/28]: Task 4 [4GHz] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --frequency='4GHz' --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET} --frequency='4GHz' --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=DerivO3CPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=4GHz --cpu-clock=4GHz --mem-type=DDR3_1600_8x8
     cp -r m5out/ ${RESULT_DIR}/${FILE}/4_4GHz/
     ((NUM++))
 
     # HBM_1000_4H_1x64, DerivO3CPU
     echo [${NUM}/28]: Task 5 [HBM_1000_4H_1x64, DerivO3CPU] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --mem_type='HBM_1000_4H_1x64' --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET} --mem_type='HBM_1000_4H_1x64' --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=DerivO3CPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=HBM_1000_4H_1x64
     cp -r m5out/ ${RESULT_DIR}/${FILE}/5_HBM_DerivO3/
     ((NUM++))
 
     # HBM_1000_4H_1x64, MinorCPU
     echo [${NUM}/28]: Task 6 [HBM_1000_4H_1x64, MinorCPU] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --mem_type='HBM_1000_4H_1x64' --cpu_type='MinorCPU' --cmd=${SRC_DIR}/${FILE}
+    # build/X86/gem5.opt ${TARGET} --mem_type='HBM_1000_4H_1x64' --cpu_type='MinorCPU' --cmd=${SRC_DIR}/${FILE}
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=MinorCPU \
+        --l1d_size=64kB --l1i_size=64kB --caches --l2_size=2MB --l2cache \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=HBM_1000_4H_1x64
     cp -r m5out/ ${RESULT_DIR}/${FILE}/6_HBM_Minor/
     ((NUM++))
 
     # No L2 cache
     echo [${NUM}/28]: Task 7 [No L2 cache] -\> $FILE
-    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --disable_l2
+    # build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --disable_l2
+    build/X86/gem5.opt ${TARGET} --cmd=${SRC_DIR}/${FILE} --cpu-type=MinorCPU \
+        --l1d_size=64kB --l1i_size=64kB --caches \
+        --sys-clock=1GHz --cpu-clock=1GHz --mem-type=DDR3_1600_8x8
     cp -r m5out/ ${RESULT_DIR}/${FILE}/7_no_l2
     ((NUM++))
 
